d6c47a70bf9f16be9269ba81f4b7c39ac5226a9f
Samuel Jentsch <sjentsch@Samuels-MBP.attlocal.net>
2020-03-14 18:55:08 -0500

    Added handlers.

--- server.js
+++ server.js
@@ -8,8 +8,22 @@ const confirmedCasesPath = path.join(__dirname, 'data', 'covid-confirmed-series.
 const deathCasesPath = path.join(__dirname, 'data', 'covid-deaths-series.json');
 const recoveredCasesPath = path.join(__dirname, 'data', 'covid-recovered-series.json');
 
-app.get('/series/confirmed', (req, res) => res.sendFile(confirmedCasesPath));
-app.get('/series/deaths', (req, res) => res.send(deathCasesPath));
-app.get('/series/recovered', (req, res) => res.send(recoveredCasesPath));
+/**
+ * Handlers for the time series files.
+ */
+app.get('/series/confirmed', (req, res) => {
+    console.log('Processing request for confirmed cases time series.');
+    res.sendFile(confirmedCasesPath)
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/series/deaths', (req, res) => {
+    console.log('Processing request for deaths time series.');
+    res.sendFile(deathCasesPath)
+});
+
+app.get('/series/recovered', (req, res) => {
+    console.log('Processing request for recovered time series.');
+    res.sendFile(recoveredCasesPath)
+});
+
+app.listen(port, () => console.log(`Listening on ${port}...`));
