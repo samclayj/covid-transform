906d458bd5b1004a7681a479281608ae0c8b4df5
Samuel Jentsch <sjentsch@Samuels-MBP.attlocal.net>
2020-03-15 09:50:54 -0500

    Add support for sending files and config

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const path = require('path');
+const cors = require('cors');
 
 const app = express()
+// This is now enabled for all origins, lock this down.
+app.use(cors());
 const port = 3000
 
 const confirmedCasesPath = path.join(__dirname, 'data', 'covid-confirmed-series.json');
 const deathCasesPath = path.join(__dirname, 'data', 'covid-deaths-series.json');
 const recoveredCasesPath = path.join(__dirname, 'data', 'covid-recovered-series.json');
+const configPath = path.join(__dirname, 'data', 'config.json');
 
 /**
  * Handlers for the time series files.
@@ -24,6 +28,11 @@ app.get('/series/deaths', (req, res) => {
 app.get('/series/recovered', (req, res) => {
     console.log('Processing request for recovered time series.');
     res.sendFile(recoveredCasesPath)
+});
+
+app.get('/config', (req, res) => {
+    console.log('Processing request for configuration file.');
+    res.sendFile(configPath)
 });
 
 app.listen(port, () => console.log(`Listening on ${port}...`));
